2d039568c92ef817de116ff4cfbe6a0872a69fd2
zumaku <60200121101@uin-alauddin.ac.id>
2024-05-15 11:13:23 +0800

    Widget Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,41 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int nilai = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         
+        
         appBar: AppBar(
-          title: Text("Aplikasiku"),
+          title: Center(child: Text("Aplikasiku")),
         ),
 
         body: Center(
-          child: Text("Hello World")
+          child: Text("Nilai Sekarang $nilai"),
         ),
+
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              nilai++;
+            }
+          );
+        }),
+
+        
       ),
     );
   }
 }
-
