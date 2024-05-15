e4583ee4c208743a8b72a62603ff143e2029bc92
zumaku <60200121101@uin-alauddin.ac.id>
2024-05-15 11:37:28 +0800

    Widget Container

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,48 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int nilai = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        
-        
         appBar: AppBar(
-          title: Center(child: Text("Aplikasiku")),
+          title: Center(
+            child: Text("Tes Container"),
+          ),
+          backgroundColor: Colors.deepOrange,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            // height: 5,
+            fontSize: 24,
+          ),
         ),
+        body: Container(
+          color: Colors.deepOrange,
 
-        body: Center(
-          child: Text("Nilai Sekarang $nilai"),
+          child: Container(
+            margin: EdgeInsets.all(20),
+            color: Colors.amber,
+
+            child: Container(
+              margin: EdgeInsets.all(20),
+              color: Colors.redAccent,
+
+              child: Container(
+                margin: EdgeInsets.all(20),
+                color: Colors.blueAccent,
+
+                child: Container(
+                  margin: EdgeInsets.all(20),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ),
         ),
-
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              nilai++;
-            }
-          );
-        }),
-
-        
       ),
     );
   }
