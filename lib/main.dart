1b2d8ced2e150feb3cfedcc781b7a76f8990623a
zumaku <60200121101@uin-alauddin.ac.id>
2024-05-22 10:42:45 +0800

    Row Widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,59 @@ class MyApp extends StatelessWidget {
           title: Text("Aplikasi Container"),
           backgroundColor: Colors.blue,
         ),
-        body: Container(
-          width: 200,
-          height: 100,
-          // padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
-          padding: EdgeInsets.all(10),
-          // color: Colors.blue,
 
-          // Decoration
-          decoration: BoxDecoration(
-            color: Colors.yellow,
-            shape: BoxShape.circle,
-            border: Border.all(color: Colors.orange, width: 3)
-
-          ),
-
-
-          child: Text(
-            "Hallo",
-            style: TextStyle(
-              fontSize: 45,
-
+        body: Row(
+          // Flex Box
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
             ),
-          )
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ],
         ),
+
+
+
+
+
+
+        // body: Container(
+        //   width: 200,
+        //   height: 100,
+        //   // padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
+        //   padding: EdgeInsets.all(10),
+        //   // color: Colors.blue,
+
+        //   // Decoration
+        //   decoration: BoxDecoration(
+        //     color: Colors.yellow,
+        //     shape: BoxShape.circle,
+        //     border: Border.all(color: Colors.orange, width: 3)
+
+        //   ),
+
+
+        //   child: Text(
+        //     "Hallo",
+        //     style: TextStyle(
+        //       fontSize: 45,
+
+        //     ),
+        //   )
+        // ),
       ),
     );
   }
