29532c20521dc9864ae499e2dd1ffdea37a0beba
zumaku <60200121101@uin-alauddin.ac.id>
2024-05-22 10:04:57 +0800

    Ndak tau apa ini

--- lib/main.dart
+++ lib/main.dart
@@ -12,39 +12,55 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Center(
-            child: Text("Nome 3"),
-          ),
-          backgroundColor: Colors.green,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            // height: 5,
-            fontSize: 24,
-          ),
+          title: Text("Aplikasi Container"),
         ),
         body: Container(
-          child: Container(
-            margin: EdgeInsets.all(100),
-            color: Colors.green,
-
-            child: Center(
+          width: double.infinity, // Set the container to take the full width of the screen
+          height: double.infinity, // Set the container to take the full height of the screen
+          color: Color.fromRGBO(250, 250, 250, 1),
+          child: Center(
+            child: Container(
+              margin: EdgeInsets.all(20),
+              color: Color.fromARGB(255, 243, 33, 131),
               child: Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Center(
-                  child: Text(
-                    "Zul Fadli Ahmad", 
-                    textAlign: TextAlign.center,
-                  )
+                margin: EdgeInsets.all(20),
+                color: const Color.fromARGB(255, 136, 26, 18),
+                child: Container(
+                  margin: EdgeInsets.all(20),
+                  color: Colors.green,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.blue,
+                          child: Center(
+                            child: Text(
+                              'Aqila Zahrah',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.red,
+                          child: Center(
+                            child: Text(
+                              '60200121115',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
-
           ),
         ),
       ),
