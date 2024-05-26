a4139d37c77db2743a6a47776c6e79b1833a19f7
zumaku <60200121101@uin-alauddin.ac.id>
2024-05-26 21:35:13 +0800

    Soal 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,89 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
+import 'dart:ui';
+void main() async {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Container Example')),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(20.0), // Memberikan ruang di sekitar konten dalam Container.
-            margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0), // Memberikan ruang di sekitar Container dari widget lain.
-            decoration: BoxDecoration(
-              color: Colors.blue, // Menentukan warna latar belakang Container.
-              shape: BoxShape.circle, // Menentukan bentuk Container menjadi lingkaran.
-              border: Border.all(
-                color: Colors.black, // Menentukan warna border.
-                width: 2.0, // Menentukan lebar border.
-              ),
-            ),
-            child: Text(
-              'Hello World',
-              style: TextStyle(color: Colors.white),
+    return Card(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 4,
+            child: Column(
+              children: [
+                Text(
+                  'Strawberry Pavlova',
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed metus dui, luctus non ante a, convallis vulputate lacus. Aenean suscipit ultrices lorem, id ultrices ligula tristique vel. Ut justo lectus, sodales in commodo vel'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.star, color: Colors.black),
+                        Icon(Icons.star, color: Colors.black),
+                        Icon(Icons.star, color: Colors.black),
+                        Icon(Icons.star, color: Colors.black),
+                        Icon(Icons.star, color: Colors.black),
+                      ],
+                    ),
+                    Text(
+                      '170 Reviews',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.kitchen, color: Colors.green[500]),
+                          Text('PREP:'),
+                          Text('25 min'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.timer, color: Colors.green[500]),
+                          Text('COOK:'),
+                          Text('1 hr'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.restaurant, color: Colors.green[500]),
+                          Text('FEEDS:'),
+                          Text('4-6'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
+        ],
       ),
     );
   }
