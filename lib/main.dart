6e4d271d1a9e8f13903b9ab263a6cddc6e80bf06
zumaku <60200121101@uin-alauddin.ac.id>
2024-05-26 20:30:36 +0800

    Soal 1 LP 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key}); // Tambahkan titik koma di sini
-
-  @override
-  Widget build(BuildContext context) {
-    // Perbaiki sintaks di sini
-    return SafeArea(
-        child: Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(10),
-            child: Center(
-              child: Text(
-                "Farm House Lembang",
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontSize: 24,
-                ),
-              )
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Container Example')),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(20.0), // Memberikan ruang di sekitar konten dalam Container.
+            margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0), // Memberikan ruang di sekitar Container dari widget lain.
+            decoration: BoxDecoration(
+              color: Colors.blue, // Menentukan warna latar belakang Container.
+              shape: BoxShape.circle, // Menentukan bentuk Container menjadi lingkaran.
+              border: Border.all(
+                color: Colors.black, // Menentukan warna border.
+                width: 2.0, // Menentukan lebar border.
+              ),
+            ),
+            child: Text(
+              'Hello World',
+              style: TextStyle(color: Colors.white),
             ),
           ),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
