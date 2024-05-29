554562f41a6d27d0ffc676a58f6d2c0d73551c20
zumaku <60200121101@uin-alauddin.ac.id>
2024-05-29 11:25:25 +0800

    RESET!

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.amber[300],
         ),
         body: Center(
-          child: Container(
-            width: 300,
-            height: 300,
-            color: Colors.amber[50],
-            // child: Image.asset("img/js_art.jpeg"),  // Cara 1
-            // child: Image(image: AssetImage("img/js_art.jpeg")),  // Cara 2
-            child: Image.network(
-              "https://akcdn.detik.net.id/community/media/visual/2024/05/08/presiden-joko-widodo-jokowi_169.jpeg?w=700&q=90",
-              fit: BoxFit.cover,
-            )
-          )
+          
         )
       ),
     );
