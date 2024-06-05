4be299985a283625e7185e77805327729c4aaa00
zumaku <60200121101@uin-alauddin.ac.id>
2024-06-05 11:48:40 +0800

    Selesaikan aplikasi  wisata bandung

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/detail.screen.dart';
+import 'package:flutter_application/main_screen.dart';
 
 void main() => runApp(const MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
+      home: MainScreen(),
     );
   }
 }
