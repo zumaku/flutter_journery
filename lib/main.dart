26e36ab6f825593e8185f8551318f56bd0c52032
zumaku <60200121101@uin-alauddin.ac.id>
2024-06-12 10:55:50 +0800

    Bad practive to State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/pages/page1.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isRed = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Page1(),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('State Management Profider'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            AnimatedContainer(
+              duration: Duration(milliseconds: 100),
+              width: 100,
+              height: 100,
+              color: isRed ?Colors.red[600] : Colors.green[600],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Green"),
+                Switch(
+                  value: isRed,
+                  onChanged: (value) => {
+                    setState(() {
+                      isRed = value;
+                    })
+                  },
+                ),
+                Text("Red"),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
