b7f374d5295f1b79817d4f8e63d1021e48d11135
zumaku <60200121101@uin-alauddin.ac.id>
2024-06-14 11:17:19 +0800

    Asistensi Laporan 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/applicationColor.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: ChangeNotifierProvider<ApplicationColor>(
         create: (BuildContext context) => ApplicationColor(),
-        child: Scaffold(
-          //AppBar
-          appBar: AppBar(
-            title: Consumer<ApplicationColor>(
-              builder: (BuildContext context, applicationColor, _) => Text(
-                "State Management Provider", 
-              style: TextStyle(
-                color: applicationColor.color, 
-              ),),
-            ),
+        child: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _width = 100;
+  int _height = 100;
+  Color _color = Colors.blue;
+
+  final TextEditingController _widthController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _colorController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _widthController.addListener(() {
+      setState(() {
+        _width = int.tryParse(_widthController.text) ?? 100;
+      });
+    });
+    _heightController.addListener(() {
+      setState(() {
+        _height = int.tryParse(_heightController.text) ?? 100;
+      });
+    });
+    _colorController.addListener(() {
+      setState(() {
+        final hexColor = _colorController.text;
+        if (hexColor.length == 6 || (hexColor.length == 7 && hexColor.startsWith('#'))) {
+          final colorString = hexColor.startsWith('#') ? hexColor.substring(1) : hexColor;
+          try {
+            _color = Color(int.parse('FF$colorString', radix: 16));
+          } catch (e) {
+            _color = Colors.blue;
+          }
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: Container(
+            width: _width.toDouble(),
+            height: _height.toDouble(),
+            color: _color,
           ),
-          //Body
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //Consumer for AnimatedContainer
-                Consumer<ApplicationColor>(
-                  builder: (context, applicationColor, _) => AnimatedContainer(
-                    width: 100,
-                    height: 100,
-                    color: applicationColor.color,
-                    duration: Duration(milliseconds: 500),
-                  ),
-                ),
-                //Row
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //Text
-                    Text("AB"), //Amber
-                    //Consumer for Switch
-                    Consumer<ApplicationColor>(
-                      builder: (BuildContext context, applicationColor, _) => Switch(
-                        value: applicationColor.isLightBlue,
-                        onChanged: (value) {
-                          applicationColor.toggleColor(value);
-                        },
-                      ),
-                    ),
-                    //Text
-                    Text("LB"), //LightBlue
-                  ],
-                )
-              ],
-            ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Width: $_width"),
+              Text("Height: $_height"),
+              TextField(
+                controller: _widthController,
+                decoration: const InputDecoration(labelText: 'Width'),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _width = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              TextField(
+                controller: _heightController,
+                decoration: const InputDecoration(labelText: 'Height'),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _height = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              TextField(
+                controller: _colorController,
+                decoration: const InputDecoration(labelText: 'Color (Hex) #'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+class ApplicationColor extends ChangeNotifier {
+  Color _color = Color(0xFF2196F3);
+
+  Color get color => _color;
+
+  void changeColor(Color newColor) {
+    _color = newColor;
+    notifyListeners();
   }
 }
